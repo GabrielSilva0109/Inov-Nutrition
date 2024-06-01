569ae68fbf4838fed95e4e7730e6a9bc5e102510
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-06-01 16:00:51 -0300

    q

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,33 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    gap: 20px;
 `
 
+const Text = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  color: black;
+
+`
+
+const Dev = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  color: black;
+  cursor: pointer;
+  transition: 1s;
+
+  &:hover{
+    color: blue;
+  }
+`
 const Footer = () => {
   return (
         <Container>
+          <Text>©CopyRight Inovi Nutrition</Text>
+          <span>|</span>
+          <Dev>@Gabriel Silva</Dev>
         </Container>    
   )
 }
