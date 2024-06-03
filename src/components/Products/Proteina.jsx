2924dead28ae23373ed4c63f7d7568005970e132
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-03 13:16:39 -0300

    1

--- src/components/Products/Proteina.jsx
+++ src/components/Products/Proteina.jsx
@@ -2,12 +2,51 @@ import React from 'react'
 import styled from 'styled-components'
 import Header from '../Header/Header'
 
+import wChoc from '../../Img/Produts/3wChoc.png'
+
 const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
     height: 400px;
+`
+
+const Left = styled.div`
+    width: 50%;
+
+    @media(max-width: 768px){
+        width: 80%;
+    }
+`
+
+const Right = styled.div`
+    width: 50%;
+ 
+    @media(max-width: 768px){
+        width: 80%;
+    }
+`
+
+const Title = styled.h1`
+    font-size: 2.5rem;
+    background-image: linear-gradient(-225deg, rgb(255 255 255) 0%, #fffb7c 51%, #d1d51e 100%);
+    -webkit-background-clip: text;
+    color: transparent;
+`
+
+const Text = styled.h3`
+    font-size: 1.1rem;
+    text-align: justify;
+    color: #dbdbd;
+    padding: 0px 40px;
+
+    @media(max-width: 768px){
+        padding: 0;
+    }
+`
+
+const Img = styled.img`
+    width: 400px;
 `
 
 
@@ -15,8 +54,26 @@ const Proteina = () => {
   return (
     <>
     <Container>
-    
-        
+      <Left>
+        <Title>Whey 3W</Title>
+        <Text>whey sei la oq </Text>
+      </Left>
+      <Right>
+        <Img src={wChoc}/>
+      </Right>
+      
+    </Container>
+    <Container>
+      <Right>
+        <Img src={wChoc}/>
+      </Right>
+
+      <Left>
+        <Title>Whey 3W</Title>
+        <Text>whey sei la oq </Text>
+      </Left>
+      
+      
     </Container>
 
     </>
